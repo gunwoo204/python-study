dc5025faa6620dc04fedc8e4a00032c72ee1568b
gunwoo204 <gunwoo204@unist.ac.kr>
2018-09-04 20:34:41 +0900

    02.HelloWorld!

---
+++ 02. Hello World!/02..py
@@ -0,0 +1,9 @@
+print("Hello, World!")  # Note that print is a function
+
+# Note that print is a function
+print('Hello, World!')
+
+'''
+Note that print is a function
+'''
+print('Hello, World!')
---
+++ 02. Hello World!/03.py
@@ -0,0 +1,5 @@
+age = 20
+name = 'Swaroop'
+
+print(f'{name} was {age} years old when he wrote this book')  # notice the 'f' before the string
+print(f'Why is {name} playing with that python?')  # notice the 'f' before the string
